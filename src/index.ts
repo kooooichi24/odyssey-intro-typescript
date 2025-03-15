9187dbe6ea193e43319d6bd150b3ac311ea9fa46
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-15 18:41:32 +0900

    lesson5: Build apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-// TODO
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import { gql } from "graphql-tag";
+import type { DocumentNode } from "graphql";
+
+const typeDefs = gql(
+  readFileSync(path.resolve(__dirname, "./schema.graphql"), {
+    encoding: "utf-8",
+  })
+);
+
+async function startApolloServer(typeDefs: DocumentNode) {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer(typeDefs);
