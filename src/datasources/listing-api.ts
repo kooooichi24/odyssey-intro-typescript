1f3c2ff0c3e466259c04088b34850fe7871b3538
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-15 20:07:22 +0900

    lesson9: Import `Listing` type generated by graphql-code-gen

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,10 +1,10 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import type { Listing } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
 
   getFeaturedListings() {
-    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-    return this.get<any[]>("featured-listings");
+    return this.get<Listing[]>("featured-listings");
   }
 }
