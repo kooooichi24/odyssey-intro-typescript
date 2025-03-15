a05e4fd1aa66335ff6b9a06e88b457f9f4ea0250
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-15 20:07:11 +0900

    lesson9: Import `Resolvers` type generated by graphql-code-gen

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,6 @@
-export const resolvers = {
+import type { Resolvers } from "./types";
+
+export const resolvers: Resolvers = {
   Query: {
     featuredListings: (_parent, _args, { dataSources }) => {
       return dataSources.listingAPI.getFeaturedListings();
