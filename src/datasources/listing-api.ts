f92a65bd5e56764d4b73cbf32f36cec11b66ef5a
kooooichi24 <ex.kouichi1224@gmail.com>
2025-03-16 18:48:30 +0900

    feat: Using a data loader to solve N+1 problem [DataLoader_Lesson-4]

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,8 +1,22 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import DataLoader from "dataloader";
 import type { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+  private batchAmenities = new DataLoader(
+    async (listingIds: string[]): Promise<Amenity[][]> => {
+      console.log("Making one batched call with ", listingIds);
+      const amenitiesLists = await this.get<Amenity[][]>("amenities/listings", {
+        params: {
+          ids: listingIds.join(","),
+        }
+      })
+      console.log(amenitiesLists);
+
+      return amenitiesLists
+    }
+  )
 
   listFeaturedListings(): Promise<Listing[]> {
     return this.get<Listing[]>("featured-listings");
@@ -13,8 +27,8 @@ export class ListingAPI extends RESTDataSource {
   }
 
   getAmenities(listingId: string): Promise<Amenity[]> {
-    console.log("Making a follow-up call for amenities with ", listingId);
-    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+    console.log("Passing listing ID to the data loader: ", listingId);
+    return this.batchAmenities.load(listingId);
   }
 
   createListing(listing: CreateListingInput): Promise<Listing> {
